ef16f1c6e1d36d03597ac4c4772d75e86f105d85
Mikael Brevik <mikaelb@mikaelb.net>
2012-03-16 17:18:30 +0100

    Building ANN class

--- controllers/webann/ann/link.py
+++ controllers/webann/ann/link.py
@@ -6,7 +6,7 @@ from layer import *
 
 class Link(object):
 
-    def __init__(self,pre_layer,post_layer,topology,arc_range_min,arc_range_max,learning_rate,learning_rule):
+    def __init__(self,pre_layer,post_layer,topology="full",arc_range_min=-1.0, arc_range_max=1.0,learning_rate=0.2,learning_rule = None):
         self.pre_layer = pre_layer
         self.post_layer = post_layer
         self.topology = topology
@@ -15,28 +15,45 @@ class Link(object):
         self.learning_rate = learning_rate
         self.learning_rule = learning_rule
 
+        self.generate_arcs()
+
     def get_random_weight(self):
         return random.uniform(self.arc_range_min, self.arc_range_max)
 
-    def generate_arcs(self,pre_nodes,post_nodes,connection_prob=0.2):
+    def generate_arcs(self, connection_prob=0.4):
         """
         Generates the arcs based on the connection topology type
         Node(pre_node,post_node,current_weight,init_weight,link)
          self.arcs = [Arc(Node(),Node(),self.get_random_weight(),self.get_random_weight(),self) for i in range(n_arcs)]
         """
         self.arcs = []
-        add = lambda pre_node, post_node: self.arcs.append(Arc(pre_node,post_node,self.get_random_weight(),self.get_random_weight(),self))
+
+        pre_nodes = self.pre_layer.nodes
+        post_nodes = self.post_layer.nodes
+
+        def connect(pre_node, post_node):
+            new_arc = Arc(pre_node, post_node, self.get_random_weight(), self)
+            self.arcs.append(new_arc)
+            pre_node.outgoing.append(new_arc)
+            post_node.incomming.append(new_arc)
+
+
         if self.topology == '1-1':
-            [[add(pre_node, post_node) for j, pre_node in enumerate(pre_nodes) if i == j ] for i, post_node in enumerate(post_nodes)]
+            [[connect(pre_node, post_node) for j, pre_node in enumerate(pre_nodes) if i == j ] for i, post_node in enumerate(post_nodes)]
 
         elif self.topology == 'full':
-            [[add(pre_node, post_node) for pre_node in (pre_nodes)] for post_node in (post_nodes)]
+            [[connect(pre_node, post_node) for pre_node in (pre_nodes)] for post_node in (post_nodes)]
 
         elif self.topology == 'stochastic':
-            [[add(pre_node, post_node) for pre_node in (pre_nodes)  if random.random() < connection_prob] for post_node in (post_nodes)]
+            [[connect(pre_node, post_node) for pre_node in (pre_nodes)  if random.random() < connection_prob] for post_node in (post_nodes)]
 
         elif self.topology == 'triangulate':
-            [[add(pre_node, post_node) for j, pre_node in enumerate(pre_nodes) if i != j] for i, post_node in enumerate(post_nodes)]
+            [[connect(pre_node, post_node) for j, pre_node in enumerate(pre_nodes) if i != j] for i, post_node in enumerate(post_nodes)]
+
+
+        self.pre_layer.exiting = self.arcs
+        self.post_layer.entering = self.arcs
+
         return self.arcs
 
 def main():
